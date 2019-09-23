1d221db1877e93a17c6f88ecfc0daef37c402320
Concellia <tiyiselani.rhangani@gmail.com>
2019-09-23 15:50:06 +0200

    added sharps and flats

--- semitone.js
+++ semitone.js
@@ -2,38 +2,60 @@ class JamBuddy {
   constructor() {
     this.semitone = [
       "A",
-      "A#",
+      ["A#","Bb"],
       "B",
       "C",
-      "C#",
+      ["C#","Db"],
       "D",
-      "D#",
+      ["D#","Eb"],
       "E",
       "F",
-      "F#",
+      ["F#","Gb"],
       "G",
-      "G#"
+      ["G#","Ab"]
     ];
     this.selectedNotes = [];
   }
 
   selectNotes() {
-
-    for (let i = 1; i <= 2; i++) {
-      var randomised = Math.floor(Math.random() * this.semitone.length);
-      this.selectedNotes.push(this.semitone[randomised]);
+    let newArray =
+    this.semitone.map(function(value,index){
+      if(value.length==2){
+        for(let i = 1; i<=1;i++){
+          var randomised = Math.floor(Math.random() * 2);
+           return value = value[randomised]
+        }
+      }else{
+        return value
+      }
+    })
+      for (let i = 1; i <= 2; i++) {
+        var randomised = Math.floor(Math.random() * this.semitone.length);
+        this.selectedNotes.push(newArray[randomised]);
+    
     }
+     
 
-    return document.getElementById(
+   return document.getElementById(
       "selectedNotes"
     ).innerHTML = this.selectedNotes;
   }
 
   checkAnswer() {
+    for (var i = 0; i < this.semitone.length; i++) {
+      if (this.semitone[i][0] === this.selectedNotes[0] || this.semitone[i][1] === this.selectedNotes[0]) {
+          var index1 = i;
+      } 
+      if (this.semitone[i][0] === this.selectedNotes[1] || this.semitone[i][1] === this.selectedNotes[1]) {
+        var index2 = i;
+    } 
+  }
     let correctAnswer = Math.abs(
-      this.semitone.indexOf(this.selectedNotes[0]) -
-        this.semitone.indexOf(this.selectedNotes[1])
+       index1-index2
     );
+    
+      
+    
     let val = document.getElementById("answer").value;
     if (val == ""|| document.getElementById("selectedNotes").innerHTML == "") {
       return document.getElementById("response").innerHTML =
